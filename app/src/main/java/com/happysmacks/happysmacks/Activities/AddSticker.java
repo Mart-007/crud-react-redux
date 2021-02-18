a03091d9b9c6ea870a791dd59a694e4eb12a1e72
aljonleynes11 <40938253+aljonleynes11@users.noreply.github.com>
2021-02-18 12:00:31 +0800

    pusg

--- app/src/main/java/com/happysmacks/happysmacks/Activities/AddSticker.java
+++ app/src/main/java/com/happysmacks/happysmacks/Activities/AddSticker.java
@@ -1,6 +1,7 @@
 package com.happysmacks.happysmacks.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,16 +10,29 @@ import com.happysmacks.happysmacks.R;
 
 public class AddSticker extends AppCompatActivity {
 
+  String imagesList[] = {
+    "https://via.placeholder.com/300.png?text=StickerCategory%201",
+    "https://via.placeholder.com/300.png?text=StickerCategory%202",
+    "https://via.placeholder.com/300.png?text=StickerCategory%203",
+    "https://via.placeholder.com/300.png?text=StickerCategory%204",
+    "https://via.placeholder.com/300.png?text=StickerCategory%205",
+    "https://via.placeholder.com/300.png?text=StickerCategory%206",
+    "https://via.placeholder.com/300.png?text=StickerCategory%207",
+    "https://via.placeholder.com/300.png?text=StickerCategory%208",
+    "https://via.placeholder.com/300.png?text=StickerCategory%209"
+  };
+  RecyclerView recyclerView;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_add_sticker);
+    recyclerView = findViewById(R.id.recyclerView1);
   }
   @Override
   public void onBackPressed() {
     super.onBackPressed();
-//        Intent intent = new Intent(this, AddSticker.class);
-//        startActivity(intent);
     finishAffinity();
   }
 }
+
---
+++ app/src/main/java/com/happysmacks/happysmacks/MyAdapter.java
@@ -0,0 +1,4 @@
+package com.happysmacks.happysmacks;
+
+public class MyAdapter {
+}
